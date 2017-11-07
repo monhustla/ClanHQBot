9ea5cda4310713c14a6fd11a6a3ba1cc4bf013f3
monhustla <rdavis23344@gmail.com>
2017-11-07 13:16:31 -0600

    Update chqbot.py

--- chqbot.py
+++ chqbot.py
@@ -36,7 +36,7 @@ subreddit=bot.subreddit('space')
 
 submission=subreddit.stream.submissions()
 #keyword=['mars']
-#comment=submission.comments[0]
+comment=submission.comments
 #author=comment.author
 
 #try:
