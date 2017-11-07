d20461e2186aef72628cdfc205e61b9071d74c6d
monhustla <rdavis23344@gmail.com>
2017-11-06 18:09:09 -0600

    Create chqbot.py

---
+++ chqbot.py
@@ -0,0 +1,25 @@
+import praw
+import datetime
+import time
+
+
+bot=praw.Reddit(user_agent='MySimpleBot v0.1',
+                client_id='tMmbJ410gthhGw',
+                client_secret='WREWn4OgA4I3Wl4XVxspVleWDbg',
+                username='Clan_HQ',
+                password='Bboy123!')
+
+subreddit=bot.subreddit('space')
+
+comments=subreddit.stream.submissions()
+keyword=['mars']
+
+
+for comment in comments:
+    text=comment.title
+    info=comment.created
+    nice=datetime.datetime.fromtimestamp(float(info)).isoformat()
+    if any(x in text.lower() for x in keyword):
+           print(text+'/n'+'/n'+str(nice))
+           comment.reply('Mars is awesome')
+           time.sleep(60)
