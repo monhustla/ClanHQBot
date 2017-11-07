0f8d5cb0970b6fc25c084406eeff810e0839ef36
monhustla <rdavis23344@gmail.com>
2017-11-07 12:28:45 -0600

    Update chqbot.py

--- chqbot.py
+++ chqbot.py
@@ -1,32 +1,42 @@
 import praw
 import datetime
 import time
-import os
-
+import sched
 
 bot=praw.Reddit(user_agent='MySimpleBot v0.1',
                 client_id='tMmbJ410gthhGw',
                 client_secret='WREWn4OgA4I3Wl4XVxspVleWDbg',
-                username=myname,
-                password=password1)
+                username='Clan_HQ',
+                password='Bboy123!')
 
 subreddit=bot.subreddit('space')
 
-comments=subreddit.stream.submissions()
-keyword=['mars']
+submission=subreddit.stream.submissions()
+#keyword=['mars']
+#comment=submission.comments[0]
+#author=comment.author
 
-def recruiting_check():
-  try:
-    for comment in comments:
-      text=comment.title
-      info=comment.created
-      nice=datetime.datetime.fromtimestamp(float(info)).isoformat()
-      if any(x in text.lower() for x in keyword):
-        print(text+'/n'+'/n'+str(nice))
-        comment.reply('Mars is awesome')
-    time.sleep(60)
-            
-          
+#try:
+s=sched.scheduler(time.time, time.sleep)
+def pullthatshit(sc):
+    for comment in submission:
+        text=comment.title
+        info=comment.created
+        person=comment.author.name
+    #comment=comment.comments[0]
+    
+    #person=submission.comments[0].author
+        nice=datetime.datetime.fromtimestamp(float(info)).isoformat()
+        print(text+'\n'+'\n'+str(nice)+'\n'+person)
+    #if any(x in text.lower() for x in keyword):
+        #print(text+'\n'+'\n'+str(nice)+'\n'+person)
+            #comment.reply('Mars is awesome')
+        #time.sleep(60)
+    s.enter(60,1, pullthatshit, (sc,))
+s.enter(80, 1, pullthatshit, (s,))
+s.run()
+#except:
+#    print ('This did not work')
     #author=comments.author
     #if any(x in text.lower() for x in keyword):
     #    print (text+'/n'+'/n'+'yay''/n')
