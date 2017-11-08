4a6ac6cd72a9d2a33adcc8ef0ccd3e80a10e916b
monhustla <rdavis23344@gmail.com>
2017-11-07 20:11:48 -0600

    Update chqbot.py

--- chqbot.py
+++ chqbot.py
@@ -63,7 +63,7 @@ def pullthatshit():
       rows=cur.fetchall()
       conn.commit()
       dude=rows[0]
-      print("Here is your dude"+'\n'+dude)
+      print("Here is your dude"+'\n'+str(dude))
       print(person)
       if dude==person:
         Print ("yes")
